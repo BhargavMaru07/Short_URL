6e6f464e249b17aa85f31e969edf15f0ff0f74de
BhargavMaru07 <bhargavhamir@gmail.com>
2024-12-21 18:54:47 +0530

    some updation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const path = require("path");
 const { connectMongoDB } = require("./connection");
 const app = express();
-const {router} = require("./routes/router")
+const { router } = require("./routes/router");
 const PORT = 3000;
+
+//we need to specifies that , we use ejs as our view engine and and all ejs file are stored in views folder.....
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 //connection
 connectMongoDB("mongodb://127.0.0.1:27017/short-url");
 
 app.use(express.json());
-app.use("/url",router)
+app.use("/url", router);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server is listening on port ${PORT}`);
